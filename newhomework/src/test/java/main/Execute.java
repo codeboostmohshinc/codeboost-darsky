9b997b9719eb301fa1f603fda5303dd445fdbafa
Hasib <hasibdaafgking@yahoo.com>
2021-01-31 11:17:34 -0800

    added basic page objects

---
+++ newhomework/src/test/java/main/Execute.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.concurrent.TimeUnit;
+
+public class Execute {
+
+    public static void main (String[]args) throws InterruptedException {
+        getTemps("Fort Ann");
+        questions(3);
+
+    }
+
+    private static void getTemps(String city){
+        String website = "darksky.com";
+        String searchButton = "" ;
+        String textBox= "";
+        System.setProperty("webdriver.chrome.driver", System.getProperty("Users/adibm/Desktop/workspace/modules/newhomework/src/test/resources/chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(website);
+
+
+
+    }
+    private static void questions (int x) {
+        String website = "darksky.com";
+        String helpPageButton="";
+        String question2="";
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(website);
+
+    }
+}
